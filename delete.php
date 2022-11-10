d461916c19b53feafd9ae8e1e639e2ff77bf7ce0
Teerapong <109723478+Passerby011@users.noreply.github.com>
2022-11-10 13:43:33 +0700

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,16 @@
+<?php 
+    include("db_connect.php");
+    $id = $_GET['id'];
+
+        $sql = "DELETE FROM user WHERE id = '$id' ";
+        $result = mysqli_query($conn, $sql);
+
+        if($result){
+            echo "<script> alert('ลบข้อมูลเรียบร้อยแล้ว')</script>";
+            header("Location:show_user.php");
+
+        }else{
+            echo "<script> alert('ไม่สามารถลบข้อมูลได้')</script>";
+            header("Location:show_user.php");
+        }
+?>
